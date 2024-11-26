2a6f85b73af91c73d78d5520d67ae65f3b8780e2
vinnietito <vincentnjuguna69@gmail.com>
2024-11-26 12:58:36 +0300

    Commits

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -1,27 +1,24 @@
 import 'package:chatapp/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
- runApp(const MyApp());
+  runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
-
- // This widget is the root of your application.
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     title: 'Chat App',
-     theme: ThemeData(
-       colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
-       useMaterial3: true,
-     ),
-     home: const Homepage(),
-   );
- }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
+        useMaterial3: true,
+      ),
+      home: const Homepage(),
+    );
+  }
 }
