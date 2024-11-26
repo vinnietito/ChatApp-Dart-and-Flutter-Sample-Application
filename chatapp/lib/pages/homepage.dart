fb801011328bcf6bd6c1521759b97a3233ffd446
vinnietito <vincentnjuguna69@gmail.com>
2024-11-26 11:40:52 +0300

    Commits

---
+++ chatapp/lib/pages/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:chatapp/main.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Text(data))
+    return const Placeholder();
+  }
+}
